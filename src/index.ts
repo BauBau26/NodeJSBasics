b1ac296a0dd19a3b4b5d111f78cf3f07f3697465
BauBau26 <110334971+BauBau26@users.noreply.github.com>
2022-10-16 16:32:20 +0100

    calculate nico's height

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-
+const nico = {
+  name: "nico",
+  age: 26,
+  height: {
+    feet: 5,
+    inches: 4,
+  },
+};
+const inchesToCm = 2.54;
+const heightInCm =
+  nico.height.feet * 12 * inchesToCm + nico.height.inches * inchesToCm;
+console.log(heightInCm);
